84f799c634074fd7402768806ac5d07f2d269a81
Ravin Yadav <yravin72@gmail.com>
2025-04-28 15:51:35 +0530

    exploring get ,post and delete

--- src/app.js
+++ src/app.js
@@ -2,17 +2,26 @@ const express=require('express');
 
 const app=express();
 
+// sequence is matter when you created route handlers
+app.get("/user",(req,res)=>{
+    res.send({firstName:"ravin",lastName:"tt"})
+})
+
+app.post("/user",(req,res)=>{
+    // saving data to db
+    console.log("data saved to db")
+    res.send("database sucessfully saved to the databse")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("delted successfully")
+})
+
+// this will match all the HTTP methods (GET, POST, PUT, DELETE, etc.)
 app.use("/test",(req,res)=>{
     res.send('Hello from the server!')
 })
 
-app.use("/",(req,res)=>{
-    res.send("running the server")
-})
-
-app.use("/hello",(req,res)=>{
-    res.send("Hello the server")
-})
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
 })
