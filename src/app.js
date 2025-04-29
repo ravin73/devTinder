b3b077059a952c5ca5eb516fe88351cdbed9a119
Ravin Yadav <yravin72@gmail.com>
2025-04-29 17:44:05 +0530

    Learn middleware and use in the program

--- src/app.js
+++ src/app.js
@@ -3,23 +3,8 @@ const express=require('express');
 const app=express();
 
 // sequence is matter when you created route handlers
-app.get("/user",(req,res)=>{
-    res.send({firstName:"ravin",lastName:"tt"})
-})
-
-app.post("/user",(req,res)=>{
-    // saving data to db
-    console.log("data saved to db")
-    res.send("database sucessfully saved to the databse")
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("delted successfully")
-})
-
-// this will match all the HTTP methods (GET, POST, PUT, DELETE, etc.)
-app.use("/test",(req,res)=>{
-    res.send('Hello from the server!')
+app.use("/user",(req,res)=>{
+    
 })
 
 app.listen(3000,()=>{
