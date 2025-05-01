101ef3a0270b3824ac97d9a6777ab12a3b327031
Ravin Yadav <yravin72@gmail.com>
2025-05-01 15:23:24 +0530

    multiple route handler and their way of calculating error

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
-const express=require('express');
+const express = require('express');
 
-const app=express();
+const app = express();
 
 // sequence is matter when you created route handlers
-app.use("/user",(req,res)=>{
+app.use("/user",[(req,res,next)=>{
+    res.send("Response")
+    next()
+},(req,res,next)=>{
+    res.send("Response 2")
     
-})
+}])
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
